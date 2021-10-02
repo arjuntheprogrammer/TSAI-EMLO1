1cc14daa6a88a061a31a74133a6824b48d11bfa1
Arjun Gupta <arjuntheprogrammer@gmail.com>
2021-10-02 18:58:45 +0530

    updated test file

--- Session1/Assignment/test.py
+++ Session1/Assignment/test.py
@@ -1,30 +1,38 @@
 import os
 import pandas as pd
 
-
-def check_data_file():
-    file = 'data.zip'
-    assert not os.path.exist(file), "data file is uploaded"
+base_dir = 'Session1/Assignment/'
 
 
-def check_model_file():
-    file = 'model.h5'
-    assert not os.path.exist(file), "Model file is uploaded"
+def get_key_last_value(file, key):
+    df_metrics = pd.read_csv(file, header=0)
+    bottom = df_metrics.tail(1)
+    return bottom[key].iloc[-1]
 
 
-def check_model_acc():
-    df_metrics = pd.read_csv('metrics.csv')
-    acc_validation = df_metrics['val_accuracy']
+def test_check_data_file():
+    file = base_dir + 'data.zip'
+    assert not os.path.exists(file), "data file is uploaded"
+
+
+def test_check_model_file():
+    file = base_dir + 'model.h5'
+    assert not os.path.exists(file), "Model file is uploaded"
+
+
+def test_check_model_acc():
+    file = base_dir + 'metrics.csv'
+    acc_validation = get_key_last_value(file, 'val_accuracy')
     assert acc_validation > 0.7, "Overall accuracy is less than 70%"
 
 
-def check_dog_acc():
-    df_metrics = pd.read_csv('metrics.csv')
-    acc_dog = df_metrics['dog_acc']
+def test_check_dog_acc():
+    file = base_dir + 'metrics.csv'
+    acc_dog = get_key_last_value(file, 'dog_acc')
     assert acc_dog > 0.7, "Dog accuracy is less than 70%"
 
 
-def check_cat_acc():
-    df_metrics = pd.read_csv('metrics.csv')
-    acc_cat = df_metrics['cat_acc']
+def test_check_cat_acc():
+    file = base_dir + 'metrics.csv'
+    acc_cat = get_key_last_value(file, 'cat_acc')
     assert acc_cat > 0.7, "Cat accuracy is less than 70%"
